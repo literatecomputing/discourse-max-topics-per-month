d42fdb7261c7a2867fd68d8b1ef5e520af3ecba7
angus <angus@mcleod.org.au>
2021-03-12 10:58:22 +1100

    Working spec && frontend

---
+++ assets/javascripts/discourse/initializers/max-topics-per-month.js.es6
@@ -0,0 +1,20 @@
+import { withPluginApi } from 'discourse/lib/plugin-api';
+import discourseComputed from 'discourse-common/utils/decorators';
+ 
+export default {
+  name: 'max-topics-per-month',
+  initialize() {
+    withPluginApi('0.8.30', api => {
+      api.modifyClass('component:create-topic-button', {
+        @discourseComputed('currentUser.remaining_topics_this_month')
+        disabled(remainingTopics) {
+          if (this.siteSettings.max_topics_per_month_enabled) {
+            return Number(remainingTopics) < 1;
+          } else {
+            return this._super(...arguments);
+          } 
+        }
+      })
+    })
+  }
+}
--- plugin.rb
+++ plugin.rb
@@ -8,29 +8,24 @@
 
 enabled_site_setting :max_topics_per_month_enabled
 
-PLUGIN_NAME ||= 'max_topics_per_month'
-
 # See discourse-assign for good examples of serializer, callback, adding method
 after_initialize do
 
-  add_to_serializer(:user, :remaining_topics_this_month, false) do
-    SiteSetting.max_topics_per_month - Topic.where(user_id: object.id).where("created_at > ?", 1.month.ago).count
+  add_to_serializer(:current_user, :remaining_topics_this_month) do
+    object.remaining_topics_this_month
   end
-
-  add_to_serializer(:current_user, :saved_searches, false) do
-    (object.name)
+  
+  add_to_class(:user, :remaining_topics_this_month) do
+    SiteSetting.max_topics_per_month - Topic.where(user_id: self.id).where("created_at > ?", 1.month.ago).count
   end
-
-  require_dependency "topic"
-  class ::Topic < ActiveRecord::Base
-    rate_limit :limit_topics_per_month
-
-    def limit_topics_per_month
-      apply_per_month_rate_limit("topics", :max_topics_per_month)
-    end
-
-    def apply_per_month_rate_limit(key, method_name)
-      RateLimiter.new(self.user, "#{key}-per-month", SiteSetting.get(method_name), 1.month.to_i)
-    end
+  
+  add_to_class(:topic, :limit_topics_per_month) do
+    apply_per_month_rate_limit("topics", :max_topics_per_month)
   end
+  
+  add_to_class(:topic, :apply_per_month_rate_limit) do |key, method_name|
+    RateLimiter.new(self.user, "#{key}-per-month", SiteSetting.get(method_name), 1.month.to_i)
+  end
+  
+  Topic.rate_limit(:limit_topics_per_month)
 end
