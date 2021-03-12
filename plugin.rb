188d38b08b359790b4ea7a54a0bc2d41e292c35e
Jay Pfaffman <pfaffman@gmail.com>
2021-03-12 08:12:30 -0800

    fix remaining count

--- plugin.rb
+++ plugin.rb
@@ -14,18 +14,18 @@ after_initialize do
   add_to_serializer(:current_user, :remaining_topics_this_month) do
     object.remaining_topics_this_month
   end
-  
+
   add_to_class(:user, :remaining_topics_this_month) do
-    SiteSetting.max_topics_per_month - Topic.where(user_id: self.id).where("created_at > ?", 1.month.ago).count
+    [0, (SiteSetting.max_topics_per_month - Topic.where(user_id: self.id).where("created_at > ?", 1.month.ago).count)].max
   end
-  
+
   add_to_class(:topic, :limit_topics_per_month) do
     apply_per_month_rate_limit("topics", :max_topics_per_month)
   end
-  
+
   add_to_class(:topic, :apply_per_month_rate_limit) do |key, method_name|
     RateLimiter.new(self.user, "#{key}-per-month", SiteSetting.get(method_name), 1.month.to_i)
   end
-  
+
   Topic.rate_limit(:limit_topics_per_month)
 end
